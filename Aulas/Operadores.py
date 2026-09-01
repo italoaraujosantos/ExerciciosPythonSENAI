0b5f211f982a73c3c6a68a5b3dced518c09a4957
Italo Araujo Chaves <italoaraujosantos@gmail.com>
2026-09-01 19:58:54 -0300

    Add exemplo de Operadores.py

---
+++ Aulas/Operadores.py
@@ -0,0 +1,18 @@
+number1 = 10
+number2 = 20
+
+sum = number1 + number2
+
+sub = number1 - number2
+
+mul = number1 * number2
+
+div = number1 / number2
+
+mod = number1 % number2
+
+
+print("Soma: ",sum)
+print("Subtração: ", sub)
+print("Multiplicação: ",mul)
+print("Divisão: ",div)
